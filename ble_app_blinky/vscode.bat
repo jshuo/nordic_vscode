2455b49c01e0a75d6e7420d4805ba4295c249ffc
Jeff Shuo <jeffshuo@secuxtech.com>
2018-12-11 11:36:02 +0800

    init

--- ble_app_blinky/vscode.bat
+++ ble_app_blinky/vscode.bat
@@ -1,23 +1,23 @@
 rem Location of Nordic SDK
-set NRF_SDK=V:/Nordic/SDK/nRF5_SDK_15.0.0_a53641a
+set NRF_SDK=C:/Users/user/nordic/nRF5_SDK_15.0.0_a53641a
 
 rem Location of Nordic Command Line tools (nrfjprog) 
-set NRF_TOOLS=C:/Program Files (x86)/Nordic Semiconductor/nrf5x/bin
+set NRF_TOOLS=C:/Program Files/Nordic Semiconductor/nrf5x/bin
 
 rem location of GCC Cross-compiler https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads
-set GNU_GCC=V:/Nordic/gcc-arm-none-eabi-7-2018-q2-update-win32/bin
+set GNU_GCC=C:/Users/user/nordic/gcc-arm-none-eabi-7-2018-q2-update-win32/bin
 
 rem Location of Gnu Tools (make) https://github.com/gnu-mcu-eclipse/windows-build-tools/releases
-set GNU_TOOLS=V:/Nordic/GNU/Tools/2.11-20180428-1604/bin
+set GNU_TOOLS=C:/Users/user/nordic/GNU/Tools/2.11-20180428-1604/bin
 
 rem Location of SEGGER JLink tools
-set SEGGER_TOOLS=C:/Program Files (x86)/SEGGER/JLink_V620i
+set SEGGER_TOOLS=C:/Program Files (x86)/SEGGER/JLink_V640
 
 rem Location of java
-set JAVA=C:/Program Files (x86)/Java/jdk1.8.0_161/bin/java.exe
+set JAVA=C:/Program Files (x86)/Java/jdk1.8.0_191/bin/java.exe
 
 rem Serial numbers of nRF development boards
 set PCA10056_SN=683798348
 set PCA10040_SN=682645815
 
-"C:/Program Files/Microsoft VS Code/Code.exe" ble_app_blinky.code-workspace
+"C:/Users/user/AppData/Local/Programs/Microsoft VS Code/Code.exe" ble_app_blinky.code-workspace
